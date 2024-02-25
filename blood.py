e99b4d87dbf757cad56f037e98d2765a126cc5ff
Luann Pereira Mendes <luannpmendes@gmail.com>
2024-02-25 16:39:48 -0300

    Update blood.py

--- blood.py
+++ blood.py
@@ -1,20 +1,14 @@
-# Defining a DNA sequence
 dna_sequence = "ATCGATCGATCGATCG"
 
-# Accessing parts of the sequence
-print("First three nucleotides:", dna_sequence[:3])
-print("Last three nucleotides:", dna_sequence[-3:])
+print("Primeiros três nucleotídeos:", dna_sequence[:3])
+print("Últimos três nucleotídeos:", dna_sequence[-3:])
 
-# Counting occurrences of a specific nucleotide
-print("Number of 'A's in the sequence:", dna_sequence.count('A'))
+print("Número de 'A's na sequência:", dna_sequence.count('A'))
 
-# Length of the sequence
-print("Length of the sequence:", len(dna_sequence))
+print("Comprimento da sequência:", len(dna_sequence))
 
-# Adding new nucleotides
 dna_sequence += "ATCG"
-print("Sequence after addition:", dna_sequence)
+print("Sequência após adição:", dna_sequence)
 
-# Removing nucleotides
 dna_sequence = dna_sequence[:-4]
-print("Sequence after removal:", dna_sequence)
+print("Sequência após remoção:", dna_sequence)
